b299c24dccc29f5701b72903b1f22b0e69a53a74
cesarve77 <c@imagenproactiva.com>
2018-12-01 12:16:40 +1000

    bump version 0.0.4

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,17 +23,17 @@ const users = [
 ];
 
 const siteConfig = {
-    title: 'Test Site', // Title for your website.
-    tagline: 'A website for testing',
-    url: 'https://your-docusaurus-test-site.com', // Your website URL
-    baseUrl: '/', // Base URL for your project */
+    title: 'Mandarina', // Title for your website.
+    tagline: 'Full-stack react+apollo+graphql+prisma.io library',
+    url: 'https://cesarve77.github.io', // Your website URL
+    baseUrl: '/mandarina/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
 
     // Used for publishing and more
-    projectName: 'Mandarina',
-    organizationName: 'WebSightCreators',
+    projectName: 'mandarina',
+    organizationName: 'cesarve77',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
