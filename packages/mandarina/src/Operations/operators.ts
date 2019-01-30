ae3f06fdca73966e6cfe05b474c79c56d87c1532
cesarve77 <c@imagenproactiva.com>
2019-01-30 21:56:46 +1000

    clean packages

--- packages/mandarina/src/Operations/operators.ts
+++ packages/mandarina/src/Operations/operators.ts
@@ -1,5 +1,4 @@
-
-const get = require("lodash.get")
+import get from "lodash.get"
 
 /**
  *  take a model, and evaluate the where clause (a prisma where shape) and return if the model complain with the clause
