592bbc243fcee09c6ecf843f0b5dddb23e7648e2
cesarve77 <c@imagenproactiva.com>
2019-11-28 23:22:40 +1000

    mandarina 11

--- packages/mandarina-antd/src/index.ts
+++ packages/mandarina-antd/src/index.ts
@@ -1,20 +1,17 @@
-const AutoField: any=require("./uniforms/AutoField").default
-const ErrorsField: any=require("./uniforms/ErrorsField").default
-const AutoFields: any=require("./uniforms/AutoFields").default
-const ListField: any=require("./uniforms/ListField").default
-const ListItemField: any=require("./uniforms/ListItemField").default
-const NestField: any=require("./uniforms/NestField").default
-const TableField: any=require("./uniforms/TableField").default
-const HiddenTableField: any=require("./uniforms/HiddenTableField").default
+const AutoField: any = require("./uniforms/AutoField").default
+const ErrorsField: any = require("./uniforms/ErrorsField").default
+const AutoFields: any = require("./uniforms/AutoFields").default
+const ListField: any = require("./uniforms/ListField").default
+const ListItemField: any = require("./uniforms/ListItemField").default
+const NestField: any = require("./uniforms/NestField").default
+const TableField: any = require("./uniforms/TableField").default
+const HiddenTableField: any = require("./uniforms/HiddenTableField").default
 import ActionForm from './ActionForm'
 import ActionButton from './ActionButton'
-import {AuthCreateForm, AuthUpdateForm, AuthList, AuthListVirtualized} from './Auth'
-import {CreateForm} from './Forms'
-import {DeleteForm} from './Forms'
-import {UpdateForm} from './Forms'
+import {AuthCreateForm, AuthList, AuthListVirtualized, AuthUpdateForm} from './Auth'
+import {CreateForm, DeleteForm, UpdateForm} from './Forms'
 import {List} from './List/List'
 import {ListVirtualized} from './List/ListVirtualized'
-import {ApolloProvider, getDataFromTree} from "react-apollo";
 
 export {
     AuthCreateForm,
@@ -36,8 +33,6 @@ export {
     DeleteForm,
     CreateForm,
     UpdateForm,
-    ApolloProvider,
-    getDataFromTree
 
 }
 
