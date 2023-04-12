9e5a8f672547ccaacd482f549318c494422249ae
Cesar Ramos <cesar@hoopscrm.com>
2023-04-13 09:25:42 +1000

    bump some dependencies

--- __TEST__/app/server/prisma.ts
+++
@@ -1,9 +0,0 @@
-import {Prisma} from "./generated/prisma";
-
-const prisma = new Prisma({
-    endpoint: 'http://localhost:5577/mandarina/test',
-    secret: "7Phk8i3M29wTutl7TFRrlPM7NrGFJaxEPEOMLFoDEIlWZwUGZBYafA5QSzlrzGTd9xPK03buzmhoGZUvqGut7n9NNi2cPLfM6UmWLlg8wlm2a5mpBEIwRG9uO1xrAA3w"
-})
-
-export default prisma
-
